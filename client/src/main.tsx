178d52ea0f6324d9febf69ac19ae2463c8622622
Thompson Tong <thompsnt@amazon.co.uk>
2026-03-02 15:49:27 +0000

    style: enable Cloudscape dark mode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import '@cloudscape-design/global-styles/index.css';
+import { applyMode, Mode } from '@cloudscape-design/global-styles';
 import { SessionsProvider } from './context/SessionsContext';
+
+applyMode(Mode.Dark);
 import { App } from './App';
 
 const root = document.getElementById('root');
